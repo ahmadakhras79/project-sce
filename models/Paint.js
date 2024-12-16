2d4031a0df14de4b990086281c8a482b882197d1
MuhammedMahameed212 <muhamma11@ac.sce.ac.il>
2024-12-16 02:54:51 +0200

    fixed stars rating for paints and drywalls

--- models/Paint.js
+++ models/Paint.js
@@ -5,6 +5,7 @@ const paintSchema = new mongoose.Schema({
   quality: { type: String, required: true },
   price: { type: Number, required: true },
   image: { type: String }, // URL or path to the image
+  rating:{type: Number, default: 0, required: false }
 });
 
 module.exports = mongoose.model('Paint', paintSchema);
--- models/drywall.js
+++ models/drywall.js
@@ -5,6 +5,7 @@ const DrywallSchema = new mongoose.Schema({
   quality: { type: String, required: true },
   price: { type: Number, required: true },
   image: { type: String, required: false }, // Optional field for image URL
+  rating:{type: Number, default: 0, required: false }
 });
 
 module.exports = mongoose.model("Drywall", DrywallSchema);
